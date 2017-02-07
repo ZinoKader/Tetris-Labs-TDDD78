4cdf64eadb01f780981c60d7368aebce73f35cbd
Zino Kader <zinokad@gmail.com>
2017-02-07 06:09:04 +0100

    Implementerat score i sidopanel

--- src/lab5/Board.java
+++ src/lab5/Board.java
@@ -2,11 +2,12 @@ package lab5;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Board {
 
-    private ArrayList<BoardListener> boardListeners;
+    private List<BoardListener> boardListeners;
     private int width;
     private int height;
     private SquareType[][] squares;
@@ -16,8 +17,15 @@ public class Board {
     private int fallingX;
     private int fallingY;
 
+    private int removedRows;
+    private int score;
     private boolean isGameOver;
+
     private static final int OUTSIDE_FRAME_SIZE = 2;
+    private static final int ROW_SCORE_1 = 100;
+    private static final int ROW_SCORE_2 = 300;
+    private static final int ROW_SCORE_3 = 500;
+    private static final int ROW_SCORE_4 = 800;
 
 
     public Board(int width, int height) {
@@ -25,6 +33,7 @@ public class Board {
         this.height = height;
         this.falling = null;
         this.isGameOver = false;
+        this.score = 0;
 
         boardListeners = new ArrayList<>();
 	tetrominoMaker = new TetrominoMaker();
@@ -49,16 +58,17 @@ public class Board {
      }
 
     public void tick() {
-	//System.out.println(isRowCompleted(height - 1));
-	removeCompletedRows();
+
+        removedRows = 0;
+
 	if(isGameOver) {
 	    System.out.println("GAME OVER!");
 	    return;
 	}
 	else if(this.falling == null) {
 	    this.falling = tetrominoMaker.getPoly(rnd.nextInt(tetrominoMaker.getNumberOfTypes()));
-	    if(falling != null) { //Sometimes gets NPEs otherwise. No idea why.
-		if(falling.getWidth() == 2) { //account for O-blocks unique size
+	    if(falling != null) { //Sometimes gets NPEs otherwise, no idea why ¯\_(ツ)_/¯
+		if(falling.getWidth() == 2) { //account for O-blocks' unique size
 		    this.fallingX = (width / 2) - 1;
 		} else {
 		    this.fallingX = (width / 2) - 2;
@@ -72,6 +82,10 @@ public class Board {
 	} else {
 	    fallDown();
 	}
+
+	removeCompletedRows();
+	addScore();
+
 	notifyListeners();
     }
 
@@ -103,25 +117,47 @@ public class Board {
 	return controlRow;
     }
 
-    public void removeCompletedRows() {
+    private void removeCompletedRows() {
         for(int row = 0; row < height; row++) {
 	    if(!Arrays.asList(getRow(row)).contains(SquareType.EMPTY)) {
 	        for(int col = 0; col < width; col++) {
 		    squares[row + OUTSIDE_FRAME_SIZE][col + OUTSIDE_FRAME_SIZE] = SquareType.EMPTY;
 		}
+		removedRows++;
 		moveDownRemainingRows(row);
 	    }
 	}
 	notifyListeners();
     }
 
-    public void moveDownRemainingRows(int removefrom) {
+    private void moveDownRemainingRows(int removefrom) {
 	for(int row = removefrom - 1; 0 <= row; row--) {
 	    for(int col = 0; col < width; col++) {
 		squares[row + OUTSIDE_FRAME_SIZE + 1][col + OUTSIDE_FRAME_SIZE] = getSquare(col, row);
 	    }
 	}
 	notifyListeners();
+    }
+
+    private void addScore() {
+	switch(removedRows) {
+	    case 1:
+	        score += ROW_SCORE_1;
+	        break;
+	    case 2:
+		score += ROW_SCORE_2;
+		break;
+	    case 3:
+		score += ROW_SCORE_3;
+		break;
+	    case 4:
+		score += ROW_SCORE_4;
+		break;
+	}
+    }
+
+    public int getScore() {
+        return score;
     }
 
     public SquareType getSquare(int x, int y) {
--- src/lab5/TetrisComponent.java
+++ src/lab5/TetrisComponent.java
@@ -7,6 +7,8 @@ import java.util.EnumMap;
 
 public class TetrisComponent extends JComponent implements BoardListener {
 
+    private static final int PANEL_SIZE = 200;
+    private static final int SCORE_FONT_SIZE = 20;
     private static final int RECTANGLE_WIDTH = 30;
     private static final int RECTANGLE_HEIGHT = 30;
     private static final int BLOCK_OUTLINE_SIZE = 1;
@@ -23,9 +25,11 @@ public class TetrisComponent extends JComponent implements BoardListener {
 	super.paintComponent(g);
 	final Graphics2D g2d = (Graphics2D) g;
 
+
 	for(int row = 0; row < gameBoard.getHeight(); row++) {
 	    for(int col = 0; col < gameBoard.getWidth(); col++) {
 
+	        //Handle painting of falling and board blocks
 		if(positionIsFalling(col, row)){
 		    g2d.setColor(squareColors.get(gameBoard.getFalling().getPoly()[row - gameBoard.getFallingY()][col - gameBoard.getFallingX()]));
 		} else {
@@ -34,6 +38,24 @@ public class TetrisComponent extends JComponent implements BoardListener {
 
 		g2d.fillRect(col * RECTANGLE_WIDTH + BLOCK_OUTLINE_SIZE, row * RECTANGLE_HEIGHT + BLOCK_OUTLINE_SIZE,
 			     RECTANGLE_WIDTH - BLOCK_OUTLINE_SIZE, RECTANGLE_HEIGHT - BLOCK_OUTLINE_SIZE);
+
+
+		//Paint side panel with gradient, magic number 10 here is to paint the remaining bits of the window
+		GradientPaint gradient = new GradientPaint(gameBoard.getWidth() * RECTANGLE_WIDTH,
+							   gameBoard.getHeight() - RECTANGLE_HEIGHT, Color.orange,
+							   gameBoard.getWidth() * RECTANGLE_WIDTH + PANEL_SIZE,
+							   gameBoard.getHeight() * RECTANGLE_HEIGHT + 10, Color.gray);
+		g2d.setPaint(gradient);
+		g2d.fillRect( gameBoard.getWidth() * RECTANGLE_WIDTH, gameBoard.getHeight() - RECTANGLE_HEIGHT,
+			      PANEL_SIZE, gameBoard.getHeight() * RECTANGLE_HEIGHT + 10);
+
+
+		//Paint score text
+		g2d.setColor(Color.white);
+		g2d.setFont(new Font("Arial", Font.PLAIN, SCORE_FONT_SIZE));
+		g2d.drawString("Score: " + Integer.toString(gameBoard.getScore()),
+			       gameBoard.getWidth() * RECTANGLE_WIDTH + PANEL_SIZE / 3,  30);
+
 	    }
 	}
 
@@ -52,7 +74,7 @@ public class TetrisComponent extends JComponent implements BoardListener {
      *
      */
     @Override public Dimension getPreferredSize() {
-	return new Dimension(gameBoard.getWidth() * RECTANGLE_WIDTH, gameBoard.getHeight() * RECTANGLE_HEIGHT);
+	return new Dimension(gameBoard.getWidth() * RECTANGLE_WIDTH + PANEL_SIZE, gameBoard.getHeight() * RECTANGLE_HEIGHT);
     }
 
     @Override public void boardChanged() {
