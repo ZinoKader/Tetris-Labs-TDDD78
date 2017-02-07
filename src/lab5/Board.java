453e83ba1548f8ca45eb76b34e359dd7ff2e629d
Zino Kader <zinokad@gmail.com>
2017-02-07 05:14:12 +0100

    Tar nu bort rader som är avklarade

--- src/lab5/Board.java
+++ src/lab5/Board.java
@@ -1,6 +1,7 @@
 package lab5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Board {
@@ -34,20 +35,22 @@ public class Board {
     }
 
     public void createBoard(){
-	for(int y = 0; y < this.height + 2 * OUTSIDE_FRAME_SIZE; y++) {
-	    for(int x = 0; x < this.width + 2 * OUTSIDE_FRAME_SIZE; x++) {
-		squares[y][x] = SquareType.OUTSIDE;
+	for(int row = 0; row < this.height + 2 * OUTSIDE_FRAME_SIZE; row++) {
+	    for(int col = 0; col < this.width + 2 * OUTSIDE_FRAME_SIZE; col++) {
+		squares[row][col] = SquareType.OUTSIDE;
 	    }
 	}
 
-	for(int y = OUTSIDE_FRAME_SIZE; y < height + OUTSIDE_FRAME_SIZE; y++) {
-	    for(int x = OUTSIDE_FRAME_SIZE; x < width + OUTSIDE_FRAME_SIZE; x++) {
-		squares[y][x] = SquareType.EMPTY;
+	for(int row = OUTSIDE_FRAME_SIZE; row < height + OUTSIDE_FRAME_SIZE; row++) {
+	    for(int col = OUTSIDE_FRAME_SIZE; col < width + OUTSIDE_FRAME_SIZE; col++) {
+		squares[row][col] = SquareType.EMPTY;
 	    }
 	}
      }
 
     public void tick() {
+	//System.out.println(isRowCompleted(height - 1));
+	removeCompletedRows();
 	if(isGameOver) {
 	    System.out.println("GAME OVER!");
 	    return;
@@ -72,25 +75,6 @@ public class Board {
 	notifyListeners();
     }
 
-    public void rotate(boolean b) {
-        if(falling != null) {
-            Poly oldFalling;
-            if(b) {
-                oldFalling = falling;
-                falling = falling.rotateRight();
-                if(hasCollision()) {
-                    falling = oldFalling;
-		}
-	    } else {
-		oldFalling = falling;
-		falling = falling.rotateLeft();
-		if(hasCollision()) {
-		    falling = oldFalling;
-		}
-	    }
-	}
-    }
-
     public int getWidth() {
 	return width;
     }
@@ -109,6 +93,35 @@ public class Board {
 
     public int getFallingY() {
 	return fallingY;
+    }
+
+    private SquareType[] getRow(int row) {
+        SquareType[] controlRow = new SquareType[width];
+	for(int col = 0; col < width; col++) {
+	    controlRow[col] = getSquare(col, row);
+	}
+	return controlRow;
+    }
+
+    public void removeCompletedRows() {
+        for(int row = 0; row < height; row++) {
+	    if(!Arrays.asList(getRow(row)).contains(SquareType.EMPTY)) {
+	        for(int col = 0; col < width; col++) {
+		    squares[row + OUTSIDE_FRAME_SIZE][col + OUTSIDE_FRAME_SIZE] = SquareType.EMPTY;
+		}
+		moveDownRemainingRows(row);
+	    }
+	}
+	notifyListeners();
+    }
+
+    public void moveDownRemainingRows(int removefrom) {
+	for(int row = removefrom - 1; 0 <= row; row--) {
+	    for(int col = 0; col < width; col++) {
+		squares[row + OUTSIDE_FRAME_SIZE + 1][col + OUTSIDE_FRAME_SIZE] = getSquare(col, row);
+	    }
+	}
+	notifyListeners();
     }
 
     public SquareType getSquare(int x, int y) {
@@ -135,31 +148,46 @@ public class Board {
 	falling = null;
     }
 
+    public void rotate(boolean b) {
+        if(falling != null) {
+            Poly oldFalling;
+            if(b) {
+                oldFalling = falling;
+                falling = falling.rotateRight();
+                if(hasCollision()) {
+                    falling = oldFalling;
+		}
+	    } else {
+		oldFalling = falling;
+		falling = falling.rotateLeft();
+		if(hasCollision()) {
+		    falling = oldFalling;
+		}
+	    }
+	}
+    }
+
     public void moveLeft() {
 	fallingX--;
-
 	if(hasCollision()) {
 	    fallingX++;
 	}
-
 	notifyListeners();
     }
 
     public void moveRight() {
 	fallingX++;
-
 	if(hasCollision()) {
 	    fallingX--;
 	}
-
 	notifyListeners();
     }
 
     public boolean hasCollision() {
-	for(int y = 0; y < falling.getHeight(); y++){
-	    for(int x = 0; x < falling.getWidth(); x++){
-		if (getSquare(fallingX + x, fallingY + y) != SquareType.EMPTY &&
-		    falling.getPoly()[y][x] != SquareType.EMPTY) {
+	for(int row = 0; row < falling.getHeight(); row++){
+	    for(int col = 0; col < falling.getWidth(); col++){
+		if (getSquare(fallingX + col, fallingY + row) != SquareType.EMPTY &&
+		    falling.getPoly()[row][col] != SquareType.EMPTY) {
 		    return true;
 		}
 	    }
